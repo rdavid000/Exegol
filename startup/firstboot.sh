d07975cf925a58aae325d70c9c107b3b34019a22
Pharah <r.david000@gmail.com>
2023-02-12 23:41:17 +0100

    my

--- startup/firstboot.sh
+++ startup/firstboot.sh
@@ -3,5 +3,6 @@ export DEBIAN_FRONTEND=noninteractive
 apt update -y
 apt upgrade -y
 python3 -m pip install exegol
-exegol install
-exit 0
+printf "\n\n\n" | exegol install
+printf "\n" | exegol start --log --disable-X11 MyExegol nightly
+exegol start --log --disable-X11 -p 4446:22 -p 4447:80 MyExegol nightly&
