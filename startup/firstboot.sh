bf1ff4761e7e8acfa56b8cdeaa165e21a77b4030
Pharah <r.david000@gmail.com>
2023-02-14 23:14:39 +0100

    my

--- startup/firstboot.sh
+++ startup/firstboot.sh
@@ -3,6 +3,5 @@ export DEBIAN_FRONTEND=noninteractive
 apt update -y
 apt upgrade -y
 python3 -m pip install exegol
-printf "\n\n\n" | exegol install
-printf "\n" | exegol start --log --disable-X11 MyExegol nightly
-exegol start --log --disable-X11 -p 4446:22 -p 4447:80 MyExegol nightly&
+printf "full\n\n" | exegol install
+printf "\n\n" | exegol exec --disable-X11 -p 4446:22 -p 4447:80 MyExegol "update-rc.d ssh defaults && service ssh start && useradd -s /bin/bash -m r2d2 && echo 'r2d2:1234' | chpasswd && usermod -aG sudo r2d2 && newgrp sudo"
