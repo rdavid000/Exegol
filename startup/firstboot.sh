2b967467b977c3c751ebd9d6c1cf1072cf609adc
Pharah <r.david000@gmail.com>
2023-02-15 21:12:25 +0100

    my

--- startup/firstboot.sh
+++ startup/firstboot.sh
@@ -4,4 +4,5 @@ apt update -y
 apt upgrade -y
 python3 -m pip install exegol
 printf "full\n\n" | exegol install
-printf "full\n" | exegol exec --disable-X11 -p 4446:22 -p 4447:80 MyExegol "update-rc.d ssh defaults && service ssh start && useradd -s /bin/bash -m r2d2 && echo 'r2d2:1234' | chpasswd && usermod -aG sudo r2d2 && newgrp sudo && sed -i 's/#*X11UseLocalhost.*/X11UseLocalhost no/' /etc/ssh/sshd_config"
+printf "full\n\n" | exegol exec --disable-X11 -p 4446:22 -p 4447:80 MyExegol "update-rc.d ssh defaults && service ssh start && useradd -s /bin/bash -m r2d2 && echo 'r2d2:1234' | chpasswd && usermod -aG sudo r2d2 && newgrp sudo && sed -i 's/#*X11UseLocalhost.*/X11UseLocalhost no/' /etc/ssh/sshd_config"
+exit 0
